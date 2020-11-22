21d7bc723e23fc5992eea39155e4f037294c34c6
Petro <petro@cpetro.us>
2020-11-22 11:23:44 -0700

    Setup for puppetdb.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
-node puppet.local {
+node 'puppet.local' {
   include role::master
   file {'/etc/secret_password.txt':
-    ensure   => file,
-    content  => lookup('secret_password'),
+    ensure  => file,
+    content => lookup('secret_password'),
   }
 }
